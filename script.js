2918e3a02ec56eea943e7925193eaa299d92e233
PejeComics-py <pejecomics@gmail.com>
2026-07-16 14:47:10 -0500

    Organizing things

--- script.js
+++
@@ -1,9 +0,0 @@
-var spinnerWrapper = document.querySelector('.spinner-wrapper');
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    spinnerWrapper.style.opacity = '0';
-    setTimeout(() => {
-      spinnerWrapper.style.display = 'none';
-    }, 500);
-  }, 1000);
-});
